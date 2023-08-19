7a29b8039924580c8368ece2313aeeb41314855f
Luloide <89493595+Luloide@users.noreply.github.com>
2023-08-19 11:16:55 -0300

    taller 1 carpetas

---
+++ Laboratorios/taller1/src/main/java/aed/Funciones.java
@@ -0,0 +1,68 @@
+package aed;
+import java.lang.Math;
+
+class Funciones {
+    int cuadrado(int x) {
+        return x * x;
+    }
+
+    double distancia(double x, double y) {
+        double res = Math.sqrt((x * x) + (y* y));
+        return res;
+    }
+
+    boolean esPar(int n) {
+        // COMPLETAR
+        return false;
+    }
+
+    boolean esBisiesto(int n) {
+        // COMPLETAR
+        return false;
+    }
+
+    int factorialIterativo(int n) {
+        // COMPLETAR
+        return 0;
+    }
+
+    int factorialRecursivo(int n) {
+        // COMPLETAR
+        return 0;
+    }
+
+    boolean esPrimo(int n) {
+        // COMPLETAR
+        return false;
+    }
+
+    int sumatoria(int[] numeros) {
+        // COMPLETAR
+        return 0;
+    }
+
+    int busqueda(int[] numeros, int buscado) {
+        // COMPLETAR
+        return 0;
+    }
+
+    boolean tienePrimo(int[] numeros) {
+        // COMPLETAR
+        return false;
+    }
+
+    boolean todosPares(int[] numeros) {
+        // COMPLETAR
+        return false;
+    }
+
+    boolean esPrefijo(String s1, String s2) {
+        // COMPLETAR
+        return false;
+    }
+
+    boolean esSufijo(String s1, String s2) {
+        // COMPLETAR
+        return false;
+    }
+}
