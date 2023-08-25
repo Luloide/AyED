9adca396df9d46a8df9a842b74fa72060008cfd2
Luloide <89493595+Luloide@users.noreply.github.com>
2023-08-25 10:11:03 -0300

    taller 2, debugging terminado, falta archivos

---
+++ Laboratorios/taller2/estudiantes/src/main/java/aed/Archivos.java
@@ -0,0 +1,18 @@
+package aed;
+
+import java.util.Scanner;
+import java.io.PrintStream;
+
+class Archivos {
+    float[] leerVector(Scanner entrada, int largo) {
+        return new float[0];
+    }
+
+    float[][] leerMatriz(Scanner entrada, int filas, int columnas) {
+        return new float[0][0];
+    }
+
+    void imprimirPiramide(PrintStream salida, int alto) {
+        
+    }
+}
