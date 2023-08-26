5ee4a444626c180d28a50a386b6b7823a005af4c
Lucía <89493595+Luloide@users.noreply.github.com>
2023-08-25 21:42:25 -0300

    terminado Archivos.java

--- Laboratorios/taller2/estudiantes/src/main/java/aed/Archivos.java
+++ Laboratorios/taller2/estudiantes/src/main/java/aed/Archivos.java
@@ -5,14 +5,46 @@ import java.io.PrintStream;
 
 class Archivos {
     float[] leerVector(Scanner entrada, int largo) {
-        return new float[0];
+        int i = 0;
+        float[] salida = new float[largo];
+
+        while (i != largo){
+            salida[i] = entrada.nextFloat();
+            i++;
+        }
+        return salida;
     }
 
     float[][] leerMatriz(Scanner entrada, int filas, int columnas) {
-        return new float[0][0];
+        int i = 0;
+        int j = 0;
+        float[][] salida = new float[filas][columnas];
+        while (i != filas){
+            while(j != columnas){
+                salida[i][j] = entrada.nextFloat();
+            j++;     
+        }
+        j = 0; 
+        i++;
+    }
+        return salida;
     }
 
     void imprimirPiramide(PrintStream salida, int alto) {
-        
+        for(int i = 1; i<= alto ; i++){
+            //print spaces
+            for (int j = alto; j > i; j--){
+                salida.print(" ");
+            }
+            // print stars
+            for( int j = 1; j <= 2*i -1 ; j++){
+                salida.print("*"); 
+            }
+            // mas espacios
+            for (int j = alto; j > i; j--){
+                salida.print(" ");
+            }
+            salida.println();
+        }
     }
 }
