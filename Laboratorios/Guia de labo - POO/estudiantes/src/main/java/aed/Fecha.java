75c499f41f0c4cbece9b3a12b34c2deabcb1c1c8
Luloide <89493595+Luloide@users.noreply.github.com>
2023-09-28 15:53:10 -0300

    guia labo

--- Laboratorios/Guia de labo - POO/estudiantes/src/main/java/aed/Fecha.java
+++ Laboratorios/Guia de labo - POO/estudiantes/src/main/java/aed/Fecha.java
@@ -1,8 +1,10 @@
 package aed;
 
 public class Fecha {
+    private int dia;
+    private int mes;
     public Fecha(int dia, int mes) {
-        throw new UnsupportedOperationException("No implementada aun");
+        new Fecha(dia, mes);
     }
 
     public Fecha(Fecha fecha) {
@@ -10,24 +12,64 @@ public class Fecha {
     }
 
     public Integer dia() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return dia;
     }
 
     public Integer mes() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return mes;
     }
 
     public String toString() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return Integer.toString(dia) +  "/" + Integer.toString(mes);
     }
 
     @Override
-    public boolean equals(Object otra) {
-        throw new UnsupportedOperationException("No implementada aun");
+    public boolean equals (Object otra) {
+        // otra es null
+        boolean oen = (otra == null);
+        // clase es distinta
+        boolean cd = otra.getClass() != this.getClass();
+        if (oen || cd) {
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
     public void incrementarDia() {
-        throw new UnsupportedOperationException("No implementada aun");
+        int[] meses31 = {1,3,5,7,8,10,12};
+        int[] meses30 = {4,6,9,11};
+        if (dia == 31){
+            if(mes == 12){
+                mes = 1;
+                dia = 1;
+            }
+            else{
+                dia = 1;
+                mes++;
+            }
+        }
+        else if(dia == 28 && mes == 2){
+            mes++;
+            dia = 1;
+        }
+        else if(dia == 30){
+            for(int i =0; i<meses31.length; i++){
+                if(mes == i){
+                    dia++;
+                }
+            for(int j = 0; i < meses30.length; i++){
+                if(mes == j){
+                    dia = 1;
+                    mes++;
+                }
+            }
+            }
+        }
+        else{
+            dia++;
+        }
     }
 
     private int diasEnMes(int mes) {
