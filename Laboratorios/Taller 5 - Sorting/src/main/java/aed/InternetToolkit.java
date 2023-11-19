c949be4d2cbb0ca0fc0fb186fe3f2fe3769f2f1a
Lucía <89493595+Luloide@users.noreply.github.com>
2023-11-19 15:18:31 -0300

    Taller 5 done

--- Laboratorios/Taller 5 - Sorting/src/main/java/aed/InternetToolkit.java
+++ Laboratorios/Taller 5 - Sorting/src/main/java/aed/InternetToolkit.java
@@ -12,7 +12,7 @@ public class InternetToolkit {
             Fragment key = fragments[i];
             int j = i - 1;
 
-            // Move elements of array[0..i-1] that are greater than key to one position ahead of their current position
+            
             while (j >= 0 && fragments[j].compareTo(key) == 1) {
                 fragments[j + 1] = fragments[j];
                 j = j - 1;
